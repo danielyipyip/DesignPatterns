cebf7b60721c3a42e7b02c2a1a4ca77b9822ba3c
Daniel YIP <longyinyip@outlook.com>
2024-04-14 18:50:20 +0100

    dupdate

---
+++ ResultPattern.cs
@@ -0,0 +1,14 @@
+﻿namespace designPattern
+{
+    /// <summary>
+    /// API error response
+    /// - null checking
+    /// - exception as flow control
+    /// - result pattern
+    /// Alternative to throwing exception (exception based error handling)
+    /// Return "result object" that contains status of operation (success/ failure) & error information
+    /// </summary>
+    public class ResultPattern
+    {
+    }
+}
